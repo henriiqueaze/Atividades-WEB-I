603fd0049f261a4b9fdf7fd5fec4ffa6100c8c36
Henrique Azevedo <henriquee.aze.dev@gmail.com>
2025-04-28 16:16:13 -0300

    Removendo atividade 06

--- Atividade 06 - Catálogo de Filmes/catalogo_filmes/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import lista_filmes, cadastrar_filme, deletar_filme
-
-urlpatterns = [
-    path('', lista_filmes, name='lista_filmes'),
-    path('cadastrar/', cadastrar_filme, name='cadastrar_filme'),
-    path('deletar/<int:filme_id>/', deletar_filme, name='deletar_filme'),
-]
